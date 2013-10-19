7b24dc2c39f479c06a7be9aaf3a033b6980232e8
Michal Dabrowski <dabrowski@brillante.pl>
2013-10-19 12:11:03 +0200

    Initial lib commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# jQuery ajax wizards
+
+The library aims to wrap the multi step wizards based on ajax content. 
+If You dont need each new step to be loaded via ajax, please use other avilable jquery wizards
+plugins. It operates on jQuery defered objects execution.
+
+## Getting Started
+
+### In the browser
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/defrag/jquery-ajax-wizard/master/dist/wizards.min.js
+[max]: https://raw.github.com/defrag/jquery-ajax-wizard/master/dist/wizards.js
+
+In your web page:
+
+```html
+<script src="dist/wizards.min.js"></script>
+<div id="wizard"></div>
+
+<script>
+var wizard = new Wizard('wizard');
+wizard.addStep(new WizardStep({url: '/foo'}));
+wizard.addStep(new WizardStep({url: '/bar'}));
+wizard.render();
+</script>
+```
+
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+_Also, please don't edit files in the "dist" subdirectory as they are generated via Grunt. You'll find source code in the "lib" subdirectory!_
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2013 Michal Dabrowski  
+Licensed under the MIT license.
---
+++ spec/wizards_spec.js
@@ -0,0 +1,78 @@
+describe("WizardStep", function() {
+
+    var step;
+
+    beforeEach(function() {
+        step = new WizardStep({url: '/foobar'});
+    });
+
+    it("should be initializable with url option", function() {
+        expect(step.url).toEqual('/foobar');
+    });
+
+    it("should load the contents of given url via", function() {
+        
+        spyOn($, 'ajax').andCallFake(function (req) {
+            var d = $.Deferred();
+            d.resolve('<p>ffffuuuuuu</p>');
+            return d.promise();
+        });
+
+        var asyncCallComplete = false;
+
+        runs(function() {
+            step.load().done(function() {
+                asyncCallComplete = true;
+            });
+        });
+
+        waitsFor(function() {
+            return false !== asyncCallComplete;
+        });
+
+        return runs(function() {
+            expect(step.content).toBe('<p>ffffuuuuuu</p>');
+            expect($.ajax.mostRecentCall.args[0]["url"]).toEqual('/foobar');
+        });
+
+    });
+});
+
+describe("Wizard", function() {
+
+    var wizard;
+
+    beforeEach(function() {
+        $('body').append('<div id="wizard"></div>');
+        wizard = new Wizard('wizard');
+    });
+
+    it("should not have any steps by default", function() {
+        expect(wizard.steps.length).toEqual(0);
+    });
+
+    it("should have default index of -1 before rendering", function() {
+        expect(wizard.currentIndex).toEqual(-1);
+    });
+
+    it("should be able to add teps", function() {
+        var step = new WizardStep({url: '/foobar'});
+        wizard.addStep(step);
+        expect(step.wizard).toEqual(wizard);
+        expect(wizard.steps.length).toEqual(1);
+        expect(step.id).toEqual(1);
+        var step2 = new WizardStep({url: '/foobar2'});
+        wizard.addStep(step2);
+        expect(step2.id).toEqual(2);
+        expect(wizard.steps.length).toEqual(2);
+    });
+
+    it("should render its basic skeleton template with first step", function() {        
+        var step1 = new WizardStep({url: '/foobar'});
+        wizard.addStep(step1);
+        wizard.render();
+        expect(wizard.currentStep).toEqual(step1);
+    });
+
+
+});
