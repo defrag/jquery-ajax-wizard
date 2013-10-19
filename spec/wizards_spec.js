9f2d9cb5dc39882a9c0d1c6528440cd49373f3ba
Michal Dabrowski <dabrowski@brillante.pl>
2013-10-19 14:34:46 +0200

    Adding spec for next navigation

--- spec/wizards_spec.js
+++ spec/wizards_spec.js
@@ -30,7 +30,7 @@ describe("WizardStep", function() {
             return false !== asyncCallComplete;
         });
 
-        return runs(function() {
+        runs(function() {
             expect(step.content).toBe('<p>ffffuuuuuu</p>');
             expect($.ajax.mostRecentCall.args[0]["url"]).toEqual('/foobar');
         });
@@ -83,5 +83,63 @@ describe("Wizard", function() {
         expect(step1.load).toHaveBeenCalled();
     });
 
+    it("should load next steps asynchronously by default", function() {
+        var step1 = new WizardStep({url: '/foobar1'});
+        var step2 = new WizardStep({url: '/foobar2'});
+        var step3 = new WizardStep({url: '/foobar3'});
+        wizard.addStep(step1);
+        wizard.addStep(step2);
+        wizard.addStep(step3);
+
+        var step1Complete, step2Complete, step3Complete = false;
+
+        spyOn($, 'ajax').andCallFake(function (req) {
+            var d = $.Deferred();
+            d.resolve('<p>ffffuuuuuubar1</p>');
+            return d.promise();
+        });
+
+        runs(function() {
+            wizard.render().done(function() {
+                step1Complete = true;
+            });
+        });
+        waitsFor(function() {
+            return false !== step1Complete;
+        });
+        runs(function() {
+            expect(wizard.currentStep).toEqual(step1);
+            expect(wizard.isOnLastStep()).toEqual(false);
+        });
+
+        // ------ Step2 ------ //
+        runs(function() {
+            wizard.nextStep().done(function() {
+                step2Complete = true;
+            });
+        });
+        waitsFor(function() {
+            return false !== step2Complete;
+        });
+        runs(function() {
+            expect(wizard.currentStep).toEqual(step2);
+            expect(wizard.isOnLastStep()).toEqual(false);
+        });
+
+        // ------ Step3 ------ //
+        runs(function() {
+            wizard.nextStep().done(function() {
+                step3Complete = true;
+            });
+        });
+        waitsFor(function() {
+            return false !== step3Complete;
+        });
+        runs(function() {
+            expect(wizard.currentStep).toEqual(step3);
+            expect(wizard.isOnLastStep()).toEqual(true);
+        });
+
+    });
 
 });
